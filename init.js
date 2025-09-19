0ed590be24872ca2a133330c7d4d7b40a99c8797
Arsenii Kharlanov <arsenyzp@gmail.com.com>
2025-09-19 16:34:31 +0200

    node https

--- init.js
+++ init.js
@@ -2,7 +2,8 @@
 const socketIO = require("socket.io"); // For real-time WebSocket communication
 const express = require("express"); // Web framework for Node.js
 const path = require("path"); // Utility for handling file and directory paths
-const http = require("http"); // Node.js HTTP server
+const https = require("https"); // Node.js HTTPS server
+const fs = require("fs"); // File system module for reading files
 const app = express(); // Create an Express application
 
 // Import configuration and signalling server logic
@@ -10,9 +11,14 @@ const config = require("./server/config");
 const signallingServer = require("./server/signalling-server");
 const routes = require("./server/routes");
 
+const options = {
+  key: fs.readFileSync(path.join(__dirname, "domain-key.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "domain.pem")),
+};
+
 // Get PORT from env variable else assign 3000 for development
 const PORT = config.PORT || 824;
-const server = http.createServer(app); // Create HTTP server with Express app
+const server = https.createServer(options, app); // Create HTTPS server with Express app
 
 // Set EJS as the view engine for rendering templates
 app.set("view engine", "ejs");
